b48590acef966dcb728f643f07275def144de85e
Natalie <natsetc@gmail.com>
2019-10-11 02:18:03 -0700

    create Events component

---
+++ src/components/Events.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    Link,
+    Switch,
+    useRouteMatch,
+    useParams
+  } from 'react-router-dom';
+
+class Events extends React.Component {
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default Events;
