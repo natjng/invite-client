57a59b8220f6167e70042825be8f31dfea55d28b
Natalie <natsetc@gmail.com>
2019-10-11 02:36:16 -0700

    add Events heading

--- src/components/Events.js
+++ src/components/Events.js
@@ -8,10 +8,34 @@ import {
 
 class Events extends React.Component {
     render() {
+        // let { path, url } = useRouteMatch();
         return (
-            
+            <div>
+                <h2>Events</h2>
+                
+                
+            </div>
         )
     }
 }
 
 export default Events;
+
+/* <div>
+    <Link to={`${url}/events`} >
+        Events
+    </Link> | 
+    <Link to={`${url}/hosted-events`} >
+        Events I'm Hosting
+    </Link>
+</div> */
+
+/* <Switch>
+    <Route exact path={path}>
+        <h3>My Events</h3>
+        <Events />
+    </Route>
+    <Route path={`${path}/:`}>
+        <h3>My Events</h3>
+    </Route>
+</Switch> */
