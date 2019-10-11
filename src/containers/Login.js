175762541b48c381ac7fe85601e20ea544794331
Natalie <natsetc@gmail.com>
2019-10-10 20:17:42 -0700

    add state, fetch, use user component

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import LoginInput from '../components/LoginInput';
+import User from '../components/User';
 
 class Login extends React.Component {
     state = {
-        email: ''
+        loggedIn: false,
+        email: '',
+        name: '',
+        events: [],
+        hosted_events: []
+    }
+
+    getUser = () => {
+        // configObj
+// change to method: 'POST' to use email inputted
+        fetch('http://localhost:3000/users/6')
+            .then(r => r.json())
+            .then(json => {
+                console.log(json)
+                const user = json.data.attributes
+                this.setState({loggedIn: true, email: user.email, name: user.name, events: user.events, hosted_events: user.hosted_events})
+                console.log(this.state);
+            })
     }
 
     handleOnChange = (e) => {
@@ -12,14 +30,18 @@ class Login extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        // configObj
-        // fetch()
+        this.getUser();
     }
 
     render() {
+        // debugger
         return (
             <div className="login-container">
+                {this.state.loggedIn ? 
+                <User user={this.state} /> 
+                : 
                 <LoginInput handleOnChange={this.handleOnChange} handleSubmit={this.handleSubmit} />
+                }
             </div>
         )
     }
