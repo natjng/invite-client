4392f9f5357c9948bd560c951397cd164afe6919
Natalie <natsetc@gmail.com>
2019-10-13 19:04:22 -0700

    enhance store with middleware and redux-devtools

--- src/index.js
+++ src/index.js
@@ -8,10 +8,15 @@ import rootReducer from './reducers/rootReducer';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(
+//     rootReducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(
     <Provider store={store}>
