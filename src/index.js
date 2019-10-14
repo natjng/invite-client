a7fcead50a6ace52b6ecb46d37d76e5ccacf4e25
Natalie <natsetc@gmail.com>
2019-10-13 19:02:52 -0700

    import thunk, applyMiddleware, and compose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
