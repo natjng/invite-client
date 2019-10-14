30d3640f32e47ea327aecb129845c350c433e687
Natalie <natsetc@gmail.com>
2019-10-14 02:30:42 -0700

    configure getUser fetch request

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,12 +1,22 @@
-import baseUrl from '../index';
+import {baseUrl} from '../index';
 
-export const getUser = () => {
+export const getUser = (userLoginStateObj) => {
     return dispatch => {
         dispatch({type: 'LOADING_USER'})
-        fetch(baseUrl + '/users')
-            .then(response => response.json())
-            .then(responseJSON => {
-                dispatch({type: 'ADD_CATS', cats: responseJSON.images})
+        const configObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userLoginStateObj)
+        }
+        fetch(baseUrl + '/users', configObj)
+            .then(r => r.json())
+            .then(json => {
+                const data = json.data.attributes
+                const user = {email: data.email, name: data.name, events: data.events, hosted_events: data.hosted_events}
+
+                dispatch({type: 'LOGIN_USER', user})
             })
     }
 }
