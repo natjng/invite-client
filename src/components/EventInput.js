9aa39686d8524ce2bb4fd05e9204cc59266e6da6
Natalie <natsetc@gmail.com>
2019-10-14 19:36:59 -0700

    Create EventInput component

---
+++ src/components/EventInput.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+class EventInput extends React.Component {
+    state = {
+        event: {
+            name: '', 
+            date: '',
+            start_time: '',
+            end_time: '',
+            location: '',
+            description: '',
+            details: '',
+        }
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            event: {
+                ...this.state.event,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const event = {
+            ...this.state.event, 
+            host_id: this.props.currentUser.id
+        }
+        // debugger
+
+        this.props.postEvent(event);
+        this.setState({
+            event: {
+                name: '', 
+                date: '',
+                start_time: '',
+                end_time: '',
+                location: '',
+                description: '',
+                details: '',
+                host_id: this.props.currentUser.id,
+            }
+        })
+        // redirect to home page
+    }
+
+    render() {
+        return (
+            <Container className="p-3">
+                <h2>Create an Event</h2>
+                <Form onSubmit={this.handleSubmit} >
+                    <Form.Group controlId="name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            name="name" 
+                            onChange={this.handleChange}
+                            value={this.state.event.name}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="date">
+                        <Form.Label>Date</Form.Label>
+                        <Form.Control 
+                            type="date" 
+                            name="date" 
+                            onChange={this.handleChange}
+                            value={this.state.event.date}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="start_time">
+                        <Form.Label>Start Time</Form.Label>
+                        <Form.Control 
+                            type="time"
+                            name="start_time" 
+                            onChange={this.handleChange}
+                            value={this.state.event.start_time}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="end_time">
+                        <Form.Label>End Time</Form.Label>
+                        <Form.Control 
+                            type="time"
+                            name="end_time" 
+                            onChange={this.handleChange}
+                            value={this.state.event.end_time}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="location">
+                        <Form.Label>Location</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            name="location" 
+                            onChange={this.handleChange}
+                            value={this.state.event.location}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="description">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            name="description" 
+                            onChange={this.handleChange}
+                            value={this.state.event.description}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        )
+    }
+}
+
+export default EventInput;
