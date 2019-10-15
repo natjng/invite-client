5f14de8d34abe5933f85c0949287aa948a5ae5e0
Natalie <natsetc@gmail.com>
2019-10-14 19:40:31 -0700

    Create EventsContainer, map state and dispatch

---
+++ src/containers/EventsContainer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import EventInput from '../components/EventInput';
+import Events from '../components/Events';
+import { postEvent, updateEvent } from '../actions/eventActions';
+
+class EventsContainer extends React.Component {
+    render() {
+        return (
+            <div className="events-container">
+                <EventInput postEvent={this.props.postEvent} currentUser={this.props.currentUser} />
+                <Events updateEvent={this.props.updateEvent} currentUser={this.props.currentUser} />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return { 
+        currentUser: state.currentUser,
+        events: state.events 
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        postEvent: (user) => dispatch(postEvent(user)),
+        updateEvent: (user) => dispatch(updateEvent(user)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsContainer);
