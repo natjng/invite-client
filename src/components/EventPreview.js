207122b6f5f39c7d12aa7b908a8dd801a0a4859e
Natalie <natsetc@gmail.com>
2019-10-16 01:42:43 -0700

    Map rsvpEvent dispatch, use in handleClick fn

--- src/components/EventPreview.js
+++ src/components/EventPreview.js
@@ -3,21 +3,25 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge'
 import { connect } from 'react-redux';
+import { rsvpEvent } from '../actions/eventActions';
 // import Event from './Event';
 
 class EventPreview extends React.Component {
 
     // this.props. event, updateEvent, rsvpEvent
 
-    handleClick = () => {
-        // rsvpEvent(this.props.event.id);
+    handleClick = (eventId) => {
+        const eventUserObj = {
+            event_id: eventId,
+            user_id: this.props.currentUser.id
+        }
+        this.props.rsvpEvent(eventUserObj);
         console.log('rsvp clicked');
-        
     }
 
     render() {       
 
-        const rsvpButton = this.props.currentUser.events.find(event => event.id === parseInt(this.props.event.id, 10)) ? <Button variant="light" size="sm" >Going</Button> : <Button variant="primary" size="sm" onClick={this.handleClick} >RSVP</Button>
+        const rsvpButton = this.props.currentUser.events.find(event => event.id === parseInt(this.props.event.id, 10)) ? <Button variant="light" size="sm" >Going</Button> : <Button variant="primary" size="sm" onClick={() => this.handleClick(this.props.event.id)} >RSVP</Button>
 
         return (
             <Card style={{ width: '30rem' }}>
@@ -28,6 +32,8 @@ class EventPreview extends React.Component {
 
                     <Card.Text>{this.props.event.attributes.date ? `${this.props.event.attributes.date}, start_time` : ''}</Card.Text>
                     <Card.Text>date, start_time <br/> location <br/> <Card.Link href="#">Details</Card.Link> </Card.Text>
+
+                    {/* ^^^ use react rouer Link tag for 'Details' link */}
 
                     {/* {this.props.event.attributes.guests.filter(guest => guest.id === this.props.currentUser.id) ? "RSVP'd" : <Button onClick={this.handleClick} >RSVP</Button>} */}
                     {this.props.currentUser.hosted_events.find(e => e.id === parseInt(this.props.event.id, 10)) ? <h5><Badge variant="light">Event Host</Badge></h5> : rsvpButton }
@@ -43,4 +49,10 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(EventPreview);
+const mapDispatchToProps = dispatch => {
+    return {
+        rsvpEvent: (eventId) => dispatch(rsvpEvent(eventId))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventPreview);
