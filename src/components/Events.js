aa7ba465e5adce4f1f7c5a4b4790014bfb83f700
Natalie <natsetc@gmail.com>
2019-10-15 20:46:41 -0700

    Show different event rsvp status

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,31 +1,53 @@
 import React from 'react';
-import Event from './Event';
-import {
-    Link,
-    Switch,
-    useRouteMatch,
-    useParams
-  } from 'react-router-dom';
+import EventPreview from './EventPreview';
+import { connect } from 'react-redux';
+
+// import {
+//     Link,
+//     Switch,
+//     useRouteMatch,
+//     useParams
+//   } from 'react-router-dom';
 
 class Events extends React.Component {
     render() {
         // let { path, url } = useRouteMatch();
-        const events = this.props.currentUser.events.map(e => <Event key={e.id} event={e} updateEvent={this.props.updateEvent} />)
 
-        const hostedEvents = this.props.currentUser.hosted_events.map(e => <Event key={e.id} event={e} updateEvent={this.props.updateEvent} />)
+        // add custom attr for event.guests_ids that is an array, use includes
+        const usersEvents = this.props.events.all.filter(event => {
+            if (event.attributes.guests.length) {
+                const eventGuests = event.attributes.guests
+                return eventGuests.filter(guest => guest.id === parseInt(this.props.currentUser.id, 10))
+            } 
+        })
+        const events = usersEvents.map(e => <EventPreview key={e.id} event={e} updateEvent={this.props.updateEvent} />)
+
+        const usersHostedEvents = this.props.events.all.filter(event => event.attributes.host.id === parseInt(this.props.currentUser.id, 10))
+        const hostedEvents = usersHostedEvents.map(e => <EventPreview key={e.id} event={e} updateEvent={this.props.updateEvent} />)
+
+        const allEvents = this.props.events.all.map(e => <EventPreview key={e.id} event={e} updateEvent={this.props.updateEvent} />)
 
         return (
             <div>
-                <h2>Events</h2>
+                <h2>Attending</h2>
                 {events}
-                <h2>Hosted Events</h2>
+                <h2>Hosting</h2>
                 {hostedEvents}
+                <h2>All Events</h2>
+                {allEvents}
             </div>
         )
     }
 }
 
-export default Events;
+const mapStateToProps = state => {
+    return {
+        currentUser: state.currentUser,
+        events: state.events
+    }
+}
+
+export default connect(mapStateToProps)(Events);
 
 /* <div>
     <Link to={`${url}/events`} >
