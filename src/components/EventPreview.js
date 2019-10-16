2c32350ee767ee251cdadc82325f6d3992c51f12
Natalie <natsetc@gmail.com>
2019-10-16 16:40:06 -0700

    Import and implement removeRsvpEvent action

--- src/components/EventPreview.js
+++ src/components/EventPreview.js
@@ -3,14 +3,12 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge'
 import { connect } from 'react-redux';
-import { rsvpEvent } from '../actions/eventActions';
+import { rsvpEvent, removeRsvpEvent } from '../actions/eventActions';
 // import Event from './Event';
 
 class EventPreview extends React.Component {
 
-    // this.props. event, updateEvent, rsvpEvent
-
-    handleClick = (eventId) => {
+    handleRsvp = (eventId) => {
         const eventUserObj = {
             event_id: eventId,
             user_id: this.props.currentUser.id
@@ -19,9 +17,23 @@ class EventPreview extends React.Component {
         console.log('rsvp clicked');
     }
 
-    render() {       
+    handleCancelRsvp = (eventUserObj) => {
+        // const eventUserObj = {
+        //     event_id: eventId,
+        //     user_id: this.props.currentUser.id
+        // }
+        this.props.removeRsvpEvent(eventUserObj);
+        console.log(eventUserObj);
+        
+        console.log('cancel rsvp');
+    }
 
-        const rsvpButton = this.props.currentUser.events.find(event => event.id === parseInt(this.props.event.id, 10)) ? <Button variant="light" size="sm" >Going</Button> : <Button variant="primary" size="sm" onClick={() => this.handleClick(this.props.event.id)} >RSVP</Button>
+    render() {       
+        const eventUser = this.props.event.attributes.event_users.find(event_user => (event_user.event_id === parseInt(this.props.event.id, 10) && event_user.user_id === parseInt(this.props.currentUser.id, 10)))
+        console.log(eventUser);
+
+        // need to change to this.props.events
+        const rsvpButton = this.props.currentUser.events.find(event => event.id === parseInt(this.props.event.id, 10)) ? <><Button variant="light" size="sm" >Going</Button><Button variant="light" size="sm" onClick={() => this.handleCancelRsvp(eventUser)} >Cancel</Button></> : <Button variant="primary" size="sm" onClick={() => this.handleRsvp(this.props.event.id)} >RSVP</Button>
 
         return (
             <Card style={{ width: '30rem' }}>
@@ -36,6 +48,8 @@ class EventPreview extends React.Component {
                     {/* ^^^ use react rouer Link tag for 'Details' link */}
 
                     {/* {this.props.event.attributes.guests.filter(guest => guest.id === this.props.currentUser.id) ? "RSVP'd" : <Button onClick={this.handleClick} >RSVP</Button>} */}
+
+                    {/* need to change to this.props.events */}
                     {this.props.currentUser.hosted_events.find(e => e.id === parseInt(this.props.event.id, 10)) ? <h5><Badge variant="light">Event Host</Badge></h5> : rsvpButton }
                 </Card.Body>
             </Card>
@@ -51,7 +65,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        rsvpEvent: (eventId) => dispatch(rsvpEvent(eventId))
+        rsvpEvent: (eventId) => dispatch(rsvpEvent(eventId)), 
+        removeRsvpEvent: (eventUsersId) => dispatch(removeRsvpEvent(eventUsersId)),
     }
 }
 
