837a9854cb63fc68ef84ad35e3e798ae96919a8a
Natalie <natsetc@gmail.com>
2019-10-17 06:17:30 -0700

    Disconnect store, add attending & hosting buttons

--- src/containers/EventsContainer.js
+++ src/containers/EventsContainer.js
@@ -1,32 +1,60 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import EventInput from '../components/EventInput';
 import Events from '../components/Events';
-import { postEvent, updateEvent } from '../actions/eventActions';
+import { useRouteMatch, Link, Switch, Route } from 'react-router-dom';
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
 
-class EventsContainer extends React.Component {
-    render() {
-        return (
-            <div className="events-container">
-                <EventInput postEvent={this.props.postEvent} currentUser={this.props.currentUser} />
-                <Events updateEvent={this.props.updateEvent} />
-            </div>
-        )
-    }
+function EventsContainer() {
+    let { path, url } = useRouteMatch();
+
+    return (
+        <div className="events-container">
+            <ButtonGroup className="mt-2">
+                <Link to={`${url}/attending`}><Button className="events-button" variant="outline-primary">Attending</Button></Link>
+                <Link to={`${url}/hosting`}><Button className="events-button" variant="outline-primary">Hosting</Button></Link>
+            </ButtonGroup>
+
+        </div>
+    )
 }
 
-const mapStateToProps = state => {
-    return { 
-        currentUser: state.currentUser,
-        events: state.events 
-    }
-}
+export default EventsContainer;
 
-const mapDispatchToProps = dispatch => {
-    return {
-        postEvent: (user) => dispatch(postEvent(user)),
-        updateEvent: (user) => dispatch(updateEvent(user)),
-    }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(EventsContainer);
+
+            {/* <Switch>
+                <Route exact path={path}>
+                    <h2>All Events</h2>
+                    <Events />
+                </Route>  
+                <Route exact path={`${path}/:eventType`}>
+                    <Events />
+                </Route>
+            </Switch> */}
+
+//                 <Switch>
+//                     <Route exact path="/events/attending">
+//                         events container
+//                         <Events events={attendingEvents} />
+//                     </Route>
+//                     {/* <Route exact path="/events/hosting">
+//                         <Events events={hostingEvents}/>
+//                     </Route> */}
+//                 </Switch>
+
+// import { connect } from 'react-redux';
+// import { updateEvent } from '../actions/eventActions';
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         updateEvent: (user) => dispatch(updateEvent(user)),
+//     }
+// }
+
+// const mapStateToProps = state => {
+//     return {
+//         currentUser: state.currentUser,
+//         events: state.events
+//     }
+// }
+
+// export default connect(mapStateToProps)(EventsContainer);
