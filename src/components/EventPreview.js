b5c3d6feece2017d39086113d54de7124bbf422e
Natalie <natsetc@gmail.com>
2019-10-17 06:13:31 -0700

    Add fn to show user event rsvp status

--- src/components/EventPreview.js
+++ src/components/EventPreview.js
@@ -4,7 +4,6 @@ import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge'
 import { connect } from 'react-redux';
 import { rsvpEvent, removeRsvpEvent } from '../actions/eventActions';
-// import Event from './Event';
 
 class EventPreview extends React.Component {
 
@@ -18,37 +17,37 @@ class EventPreview extends React.Component {
     }
 
     handleCancelRsvp = (eventUserObj) => {
-        // const eventUserObj = {
-        //     event_id: eventId,
-        //     user_id: this.props.currentUser.id
-        // }
         this.props.removeRsvpEvent(eventUserObj);        
         console.log('cancel rsvp');
     }
 
-    render() {       
+    userEventStatus = () => {
         const eventUser = this.props.event.attributes.event_users.find(event_user => (event_user.event_id === parseInt(this.props.event.id, 10) && event_user.user_id === parseInt(this.props.currentUser.id, 10)))
-        // console.log(eventUser);
 
-        // need to change to this.props.events
-        const rsvpButton = this.props.currentUser.events.find(event => event.id === parseInt(this.props.event.id, 10)) ? <><Button variant="light" size="sm" >Going</Button><Button variant="light" size="sm" onClick={() => this.handleCancelRsvp(eventUser)} >Cancel</Button></> : <Button variant="primary" size="sm" onClick={() => this.handleRsvp(this.props.event.id)} >RSVP</Button>
+        if (this.props.event.attributes.host.id === parseInt(this.props.currentUser.id, 10)) {
+            return <h5><Badge variant="light">Event Host</Badge></h5>
+        }
+        else if (this.props.event.attributes.guests.find(guest => guest.id === parseInt(this.props.currentUser.id, 10))) {
+            return (
+                <>
+                    <h5><Badge variant="light">Going</Badge></h5>
+                    <Button variant="light" size="sm" onClick={() => this.handleCancelRsvp(eventUser)} >Cancel</Button>
+                </>)
+        } else {
+            return <Button variant="primary" size="sm" onClick={() => this.handleRsvp(this.props.event.id)} >RSVP</Button>
+        }
+    }
 
+    render() {
         return (
             <Card className="event-card" style={{ width: '30rem' }}>
                 <Card.Header as="h5">{this.props.event.attributes.name}</Card.Header>
                 <Card.Body>
-                    {/* <Card.Title>{this.props.event.attributes.name}</Card.Title> */}
-                    {/* <Card.Subtitle>{this.props.event.attributes.date ? `Date: ${this.props.event.attributes.date}` : ''}</Card.Subtitle> */}
-
-                    <Card.Text>{this.props.event.attributes.date ? `${this.props.event.attributes.date}, start_time` : ''}</Card.Text>
-                    <Card.Text>date, start_time <br/> location <br/> <Card.Link href="#">Details</Card.Link> </Card.Text>
-
-                    {/* ^^^ use react rouer Link tag for 'Details' link */}
-
-                    {/* {this.props.event.attributes.guests.filter(guest => guest.id === this.props.currentUser.id) ? "RSVP'd" : <Button onClick={this.handleClick} >RSVP</Button>} */}
-
-                    {/* need to change to this.props.events */}
-                    {this.props.currentUser.hosted_events.find(e => e.id === parseInt(this.props.event.id, 10)) ? <h5><Badge variant="light">Event Host</Badge></h5> : rsvpButton }
+                    <Card.Text>
+                        {this.props.event.attributes.date ? <>{(new Date(this.props.event.attributes.date).toDateString())}<br/></> : ''}
+                        {this.props.event.attributes.location ? <>{this.props.event.attributes.location}<br/></> : ''}
+                    </Card.Text>
+                    {this.userEventStatus()}
                 </Card.Body>
             </Card>
         )
