b841607516ba14688150b15ad10b6dde3102efd3
Natalie <natsetc@gmail.com>
2019-10-16 21:34:54 -0700

    Create Dashboard

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+    // BrowserRouter as Router,
+    // Route,
+    Link,
+    // Switch
+} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+class Dashboard extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello, {this.props.currentUser.name}</h2>
+                <br />
+                <Link to="/events/new" ><Button>+ Event</Button></Link>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+      currentUser: state.currentUser
+    };
+}
+
+export default connect(mapStateToProps)(Dashboard);
