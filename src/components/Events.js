0bed6a58cc4b1a76934eb417e1ea4751d518d0a8
Natalie <49847911+natjng@users.noreply.github.com>
2019-11-22 23:33:21 -0800

    Change cmpt to fn, useParams to display event type

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import EventPreview from './EventPreview';
+import { useParams } from 'react-router-dom';
 
-class Events extends React.Component {
-    render() {
-        
-        const showEvents = this.props.events.map(e => <EventPreview key={e.id} event={e} />)
+function Events(props) {
 
-        return (
-            <div className="events-container">
-                {showEvents}
-            </div>
-        )
+    let { eventType } = useParams();
+    let eventTypeCap;
+    if (eventType) {
+        eventTypeCap = eventType.charAt(0).toUpperCase() + eventType.slice(1)
     }
+    
+    const showEvents = props.events.map(e => <EventPreview key={e.id} event={e} />)
+
+    return (
+        <div className="events-container">
+            <h2>{eventTypeCap}</h2>
+            {showEvents}
+        </div>
+    )
 }
 
 export default Events;
