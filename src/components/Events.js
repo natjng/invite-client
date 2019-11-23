49a9327ccf365baf653d8406409d7e7cb941c153
Natalie <49847911+natjng@users.noreply.github.com>
2019-11-22 23:51:52 -0800

    Use switch to show event type based on useParams

--- src/components/Events.js
+++ src/components/Events.js
@@ -6,11 +6,22 @@ function Events(props) {
 
     let { eventType } = useParams();
     let eventTypeCap;
+    let eventFilter = props.events;
     if (eventType) {
         eventTypeCap = eventType.charAt(0).toUpperCase() + eventType.slice(1)
+        switch (eventType) {
+            case 'attending':
+                eventFilter = props.attending
+                break;
+            case 'hosting':
+                eventFilter = props.hosting
+                break;
+            default:
+                eventFilter = props.events
+        }
     }
     
-    const showEvents = props.events.map(e => <EventPreview key={e.id} event={e} />)
+    const showEvents = eventFilter.map(e => <EventPreview key={e.id} event={e} />)
 
     return (
         <div className="events-container">
