4a10c5dfec4873c3be0debacae0904aef53e989a
Natalie <49847911+natjng@users.noreply.github.com>
2019-11-30 12:46:39 -0800

    Comment out eventType switch statement

--- src/components/Events.js
+++ src/components/Events.js
@@ -3,29 +3,28 @@ import EventPreview from './EventPreview';
 import { useParams } from 'react-router-dom';
 
 function Events(props) {
-
-    let { eventType } = useParams();
-    let eventTypeCap;
-    let eventFilter = props.events;
-    if (eventType) {
-        eventTypeCap = eventType.charAt(0).toUpperCase() + eventType.slice(1)
-        switch (eventType) {
-            case 'attending':
-                eventFilter = props.attending
-                break;
-            case 'hosting':
-                eventFilter = props.hosting
-                break;
-            default:
-                eventFilter = props.events
-        }
-    }
+    // let { eventType } = useParams();
+    // let eventTypeCap;
+    // let eventFilter = props.events;
+    // if (eventType) {
+    //     eventTypeCap = eventType.charAt(0).toUpperCase() + eventType.slice(1)
+    //     switch (eventType) {
+    //         case 'attending':
+    //             eventFilter = props.attending
+    //             break;
+    //         case 'hosting':
+    //             eventFilter = props.hosting
+    //             break;
+    //         default:
+    //             eventFilter = props.events
+    //     }
+    // }
     
-    const showEvents = eventFilter.map(e => <EventPreview key={e.id} event={e} />)
+    const showEvents = props.events.map(e => <EventPreview key={e.id} event={e} />)
 
     return (
         <div className="events-container">
-            <h2>{eventTypeCap}</h2>
+            {/* <h2>{eventTypeCap}</h2> */}
             {showEvents}
         </div>
     )
