4b0b1703f2216a874088758da622579d331eca41
Natalie <49847911+natjng@users.noreply.github.com>
2019-12-02 20:27:33 -0800

    Change Event fn to component, connect state

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Card from 'react-bootstrap/Card';
 
-const Event = ({ event, updateEvent }) => {
-    return (
-        <Card>
-            {event.name}
-            {event.date ? ` | ${event.date}` : ''}
-        </Card>
-    )
+class Event extends React.Component {
+
+    handleClick = () => {
+        console.log('rsvp clicked');
+        // this.props.rsvpEvent(this.props.event.id);
+    }
+
+    render() {
+        return (
+            <Card style={{ width: '30rem' }}>
+                Event Show
+                {/* <Card.Body>
+                <Card.Title>{this.props.event.name}</Card.Title>
+                {this.props.event.date ? `Date: ${this.props.event.date}` : ''}
+                </Card.Body> */}
+                <Button variant="primary" >RSVP</Button>
+            </Card>
+        )
+    }
 }
 
-export default Event;
+const mapStateToProps = state => {
+    return {
+        currentUser: state.currentUser
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        rsvpEvent: (eventId) => dispatch(rsvpEvent(eventId)), 
+        removeRsvpEvent: (eventUsersId) => dispatch(removeRsvpEvent(eventUsersId)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Event);
+// export default Event;
